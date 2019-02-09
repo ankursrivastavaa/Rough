36474b8eb8eda83066f9c3c52d98190a3274e420
ankur <ankursrivastava5544@gmail.com>
2019-02-09 01:09:55 -0500

    Comit

--- Rough_Semi_merge/Rough_Semi_merge/Program.cs
+++ Rough_Semi_merge/Rough_Semi_merge/Program.cs
@@ -37,7 +37,7 @@ namespace Rough_Semi_merge
 
 
             Console.WriteLine("Function : Calculate the Closest Numbers");
-            int[] arr3 = new int[] { 5, 4, 3, 2 };
+            int[] arr3 = new int[] { -20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470 };
             int[] answer1 = closestNumbers(arr3);
             for (int z2 = 0; z2 < answer1.Length; z2++)
             {
@@ -47,8 +47,15 @@ namespace Rough_Semi_merge
                     Console.Write(" ,");
                 }
             }
-            Console.ReadKey(true);
+            Console.WriteLine();
             Console.WriteLine("----------------------------------------------------------------------------------");
+
+            Console.WriteLine("Function : Maxcount of toys");
+            int[] p = new int[] { 1, 48, 2, 20, 20, 1 };
+            int k = 50;
+            int count = maximumToys(p, k);
+            Console.WriteLine("The maxcount of toys is = " + count);
+            Console.ReadKey(true);
 
         }// End of Main
 
@@ -227,64 +234,109 @@ namespace Rough_Semi_merge
 
 
         public static int[] closestNumbers(int [] Arr)
+
         {
             int j = 0;
             int[] store = new int [Arr.Length + 10];
 
-            Arr = Sort(Arr);
-            double oldmin = 1/.00009 ;
+            Arr = Sort(Arr);                                                    // Sorting the array in acending order
+            double oldmin = 1/.000000001 ;                                      // Initialing oldmin with a value which tends to positive infinity
 
             for (int i = 0; i < Arr.Length - 1; i++)
-            {
+                {
 
-                   double newmin = Arr[i + 1] - Arr[i];
+                    double newmin = Arr[i] - Arr[i + 1];                        // taking the differnce between first element and second element
 
-                     if (newmin < 0)
-                     {
-                       newmin = newmin * -1;
-                     }
+                    newmin = check(newmin);                                     // checking for the absolute difference
 
                     if (oldmin > newmin)
                     {
-                    oldmin = newmin;
-                    store[j] = Arr[i];
-                    j++;
-                    store[j] = Arr[i + 1];
-                     j++;
 
-                }// end of IF
+                        oldmin = newmin;
+                        store[j] = Arr[i];                                     // Storing the value of the first element in the array if the newmin is smaller than oldmin
+                        j++;                                                   
+                        store[j] = Arr[i + 1];                                  //Storing the value of the second element
+                        j++;                                                    // Incrementing the value of j by 1 to store the elements in the "store" array
+
+
+                    }// end of IF
 
                     else if (oldmin == newmin)
                     {
 
-                    store[j] = Arr[i];
-                    j++;
-                    store[j] = Arr[i + 1];
-                    j++;
+                        store[j] = Arr[i];                                      // Same logic as above described
+                        j++;
+                        store[j] = Arr[i + 1];
+                        j++;
 
-                }//  end of else if
+                    }//  end of else if
 
-                else
+                    else
                     {
                         //Do Nothing
                     }
 
-                
+                }// end of FOR 
 
-                //}//  end of FOR - 2
+                int[] final = new int[j];                               // This array will contain the final value that method return
+                int[] recheck = new int[j];                             // This array is to recheck if all the pair of elements have same value of difference or not
+
+                for (int i = 0; i < j; i++)
+                {
+                    final[i] = store[i];                                // Populating the values of store in array "final" and "check" from the "store" array
+                    recheck[i] = store[i];
+
+                }
 
 
-            }// end of FOR - 1
+                for (int i = 0; i < recheck.Length - 3; i = i + 2)                  // Hopping the value of i by 2, to pick the 1st, 3rd and so on numbers
+                {
 
-            int[] final = new int[j];
+                    int diff1 = recheck[i] - recheck[i + 1];
+                    int diff2 = recheck[i + 2] - recheck[i + 3];
+                Console.WriteLine("diff1 = " + diff1);
+                Console.WriteLine("diff2 = " + diff2);
 
-            for (int i = 0; i < j; i++)
-            {
-                final[i] = store[i];
-            }
+                if (diff1 != diff2)                                                 // Checking if the difference bwteen first pair of elements is equal to second pair
+                {
+                    //closestNumbers(recheck);                                      // Tried to call the closest number function
+
+                    Console.WriteLine("Needs to call once more the closestNumber function, but when it's called it's creating an infinite loop");
+                }
+
+                else
+                    break;
+                }
             return final;
 
         }// End of Method Closest Number
+
+
+         static double check(double n)
+        {
+            if (n < 0)
+            {
+                n = n * -1;
+                //Console.WriteLine("this is test");
+            }
+            return n;
+        }
+
+        static int maximumToys(int[] prices, int k)
+        {
+            Sort(prices);                                           // Sorting the array in order to check for the max price limit which Josh can afford
+            int maxcount = 0, sum = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if ((sum + prices[i]) < k)                          // Checking if the addition of the new number is still possible or not
+                {
+                    sum = sum + prices[i];                          // Adding all  the prices of the toys till now
+                    maxcount++;                                     // Obtaining the count of the toys bought
+                }
+            }
+            return maxcount;
+        }// End of Maxcount Function
 
 
 
